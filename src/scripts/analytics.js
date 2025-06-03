71b297cc7288945dd3a66a921e98fffe4b950c0f
mi <69121442+muhammadihabk@users.noreply.github.com>
2025-06-03 18:41:32 +0300

    feat: analytics section

---
+++ src/scripts/analytics.js
@@ -0,0 +1,31 @@
+const ctx = document.getElementById('analytics-chart');
+
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    datasets: [
+      {
+        label: '',
+        data: [11, 19, 3, 5, 2, 3, 15],
+        borderWidth: 2,
+        borderColor: '#636363',
+        tension: 0.5,
+        backgroundColor: '#636363',
+        pointBackgroundColor: '#636363',
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
